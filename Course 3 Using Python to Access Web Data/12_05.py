9ae42677d250e8feeb122650dbc22ed338537f45
Michael Olaya <michaelaolaya@gmail.com>
2018-01-16 12:35:18 -0600

    Deal with ssl errors

--- Course 3 Using Python to Access Web Data/12_05.py
+++ Course 3 Using Python to Access Web Data/12_05.py
@@ -2,9 +2,14 @@
 
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 url = input('Enter url:')
-html = urllib.request.urlopen(url).read()
+html = urllib.request.urlopen(url, context = ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
 
 # Retreive all of the anchor tags
@@ -20,3 +25,4 @@ for tag in tags:
 for i in links:
     print(i)
 print('Total links:', count)
+
